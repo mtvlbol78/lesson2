f3a990c7e58aa949293033da00a39e8b1bc6d47a
mtvlbol78 <162969203+mtvlbol78@users.noreply.github.com>
2024-03-22 13:31:42 +0300

    Add files via upload

---
+++ DZ5.py
@@ -0,0 +1,14 @@
+my_list = ['ананас', 'банан', 'яблоко', 'персик', 'груша']
+print(my_list)
+print(my_list[0],',', my_list[-1])
+print(my_list[2:5])
+my_list [2] = 'лимон'
+print(my_list)
+
+my_dict = {'ананас' : 'pineapple', 'банан': 'banana', 'яблоко' : 'apple', 'персик' : 'peach', 'груша' : 'pear'}
+print(my_dict)
+print(my_dict['банан'])
+my_dict['яблоко'] = 'Macintosh'
+my_dict.update({'кокос':'coconut'})
+print(my_dict)
+
---
+++ probe.py
@@ -0,0 +1,14 @@
+
+x, y = map(int, input().split())
+print(type(x))
+print(x,y)
+# name.find('A') буквы "А" от начала слова
+# name.rfind('A') буквы "А" от конца слова
+# name.replayce('A', 'Б') замена буквы "А" на "Б"
+# name.upper() все заглавные буквы
+# name.lower() все маленькие буквы
+# name.isalpha() проверка все ли знаки являются буквами
+# name.isnumeric() проверка все ли знаки являются цифрами
+# len(my_string) - количество символов в строке (my_string)
+
+

7f6bbf65b7fd67b2a55571f37e5ddd7dd03041cb
mihai <mihail_bol@mail.ru>
2024-03-28 21:42:55 +0300

    Ferst comit

--- HomeWork/homework6.py
+++ HomeWork/homework6.py
@@ -4,6 +4,37 @@
 #if age < 18:
 #    print('вы слишком молоды')
 
-a = 5
-if a == 5 and (a>1 or a <10):
-    print('ok')
+#a = 5
+#if a == 5 and (a>1 or a <10):
+#   print('ok')
+
+#x = 38
+#if x < 0:
+#    print('Меньше нуля')
+#print('досвидания!')
+
+#a, b = 10, 5
+#if a > b:
+#    print('a>b')
+#if a < b:
+#    print('a >b')
+#if (a > b) and (a>0 or b<1000):
+#    print('успех')
+#if 5<b and b < 10:
+#    print('успех')
+
+#if '34' > '123':
+#    print('успех')
+#if '123' > '12':
+#    print('успех')
+#if [1, 2] > [1, 1]:
+#    print('успех')
+
+#if '6' > 5:
+#   print('успех')
+# if [5, 6] > 5:
+#    print('успех')
+if '6' != 5:
+    print('успех')
+
+

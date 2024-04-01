b35a364e28aa5256ad3201c40ffaeb7e0dec346b
mihai <mihail_bol@mail.ru>
2024-04-01 23:10:43 +0300

    Ferst comit

---
+++ HomeWork/homework7.py
@@ -0,0 +1,7 @@
+auto_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for i in range(len(auto_)):
+    print('Я езжу на автомобиле', auto_[i])
+    cars_count = cars_count + 10
+    print(cars_count)
+
